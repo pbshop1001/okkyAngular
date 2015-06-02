d33081f56ca004ecfb6d1968e3a6e71161a4464c
Kevin Son <kruny1001@gmail.com>
2015-06-01 23:59:08 -0500

    progress Bar

--- public/dist/application.js
+++ public/dist/application.js
@@ -2757,6 +2757,55 @@ function mainMenu(){
 }
 'use strict';
 
+angular.module('animations').directive('statusBar', statusBar);
+
+function statusBar() {
+		return {
+			restrict: 'A',
+			controller: statusBarCtrl,
+
+			link: function postLink(scope, element, attrs) {
+				var renderer = PIXI.autoDetectRenderer(150, 700,{backgroundColor: 0xf1c40f});
+				element.append(renderer.view);
+				element.append('<div>{{title}}</div>');
+
+				var stage = new PIXI.Container();
+
+				animate();
+
+				function animate() {
+					requestAnimationFrame(animate);
+					renderer.render(stage);
+				}
+			}
+	};
+}
+
+function statusBarCtrl($scope){
+	$scope.title = 'statusBarCtrl';
+
+}
+
+
+angular.module('animations').directive('statusBarSvg', statusBarSvg);
+
+function statusBarSvg() {
+	return {
+		templateUrl:'modules/animations/directives/template/statusBarSvg.html',
+		restrict: 'E',
+		controller: statusBarSvgCtrl,
+		link: function postLink(scope, element, attrs) {
+		}
+	};
+}
+
+function statusBarSvgCtrl($scope){
+	$scope.title = 'statusBarCtrl';
+
+}
+
+'use strict';
+
 // Configuring the Articles module
 angular.module('articles').run(['Menus',
 	function(Menus) {
